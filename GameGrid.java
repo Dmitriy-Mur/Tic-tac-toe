2b158f21cd5ef2854ddfc2eccac0c6e9339791fe
Dmitriy <148892171+Dmitriy-Mur@users.noreply.github.com>
2024-05-04 11:25:34 +0500

    Add files via upload

--- GameGrid.java
+++ GameGrid.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class GameGrid {
@@ -6,40 +6,36 @@ public class GameGrid {
     private final int width;
     private final int inRowToWin;
     private final char[][] grid;
-    public HashMap<Pair, Integer> gameMap = new HashMap<>();
-
-    public static class Pair {
-        int first;
-        int second;
-
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
+    private final HashMap<Pos, Integer> gameMap ;
+    private record Pos(int height, int width) {};
+    private final boolean[] horizontalRow;
+    private final boolean[] verticalRow;
+    private final boolean[] firstDiagonalRow;
+    private final boolean[] secondDiagonalRow;
 
     public GameGrid(int height, int width, int inRowToWin) {
         this.width = width;
         this.height = height;
         this.inRowToWin = inRowToWin;
-        this.grid = new char[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                grid[i][j] = ' ';
-            }
+        gameMap = new HashMap<>();
+        grid = new char[height][width];
+        for (char[] arr : grid) {
+            Arrays.fill(arr, ' ');
         }
+        int size = inRowToWin * 2 - 1;
+        horizontalRow = new boolean[size];
+        verticalRow = new boolean[size];
+        firstDiagonalRow = new boolean[size];
+        secondDiagonalRow = new boolean[size];
     }
 
     public void print() {
-        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.print(' ');
-        if (height > 9) {
-            System.out.print(' ');
-        }
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.print("  ");
         for (int i = 0; i < width; i++) {
-            System.out.print("  " + str.charAt(i));
+            System.out.print("  " + alphabet.charAt(i));
         }
-        System.out.print('\n');
+        System.out.println();
         for (int i = 1; i <= height; i++) {
             if (i < 10) {
                 System.out.print(' ');
@@ -48,130 +44,52 @@ public class GameGrid {
             for (int j = 0; j < width; j++) {
                 System.out.print("[" + grid[i - 1][j] + "]");
             }
-            System.out.print('\n');
+            System.out.println();
         }
     }
 
-    public boolean tryPut(int height, int width, int isFirstPlayerStep) {
-        if (gameMap.containsKey(new Pair(height, width))) {
+    public boolean tryPut(int height, int width, boolean isFirstPlayerStep) {
+        if (height >= this.height || width >= this.width) {
             return false;
         }
-        gameMap.put(new Pair(height, width), isFirstPlayerStep);
+        if (gameMap.containsKey(new Pos(height, width))) {
+            return false;
+        }
+        gameMap.put(new Pos(height, width), isFirstPlayerStep ? 1 : 0);
+        grid[height][width] = isFirstPlayerStep ? 'X' : 'O';
         return true;
     }
 
-
-//    public boolean tryPut(int height, int width, boolean isFirstPlayerStep) {
-//        if (height > this.height || width > this.width) {
-//            return false;
-//        }
-//        if (grid[height][width] != ' ') {
-//            return false;
-//        }
-//        if (isFirstPlayerStep) {
-//            grid[height][width] = 'X';
-//        } else {
-//            grid[height][width] = '0';
-//        }
-//        return true;
-//    }
-//
-//    public boolean isGameOver() {
-//        int n = height;
-//        int m = width;
-//        int x = inRowToWin;
-//        // Check for horizontal wins
-//        for (int i = 0; i < n; i++) {
-//            char first = grid[i][0];
-//            if (first != ' ') {
-//                int count = 1;
-//                for (int j = 1; j < m; j++) {
-//                    if (grid[i][j] == first) {
-//                        count++;
-//                        if (count == x) {
-//                            return true;
-//                        }
-//                    } else {
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//
-//        // Check for vertical wins
-//        for (int j = 0; j < m; j++) {
-//            char first = grid[0][j];
-//            if (first != ' ') {
-//                int count = 1;
-//                for (int i = 1; i < n; i++) {
-//                    if (grid[i][j] == first) {
-//                        count++;
-//                        if (count == x) {
-//                            return true;
-//                        }
-//                    } else {
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//
-//        // Check for diagonal wins
-//        if (n == m) {
-//            char first = grid[0][0];
-//            if (first != ' ') {
-//                int count = 1;
-//                for (int i = 1; i < n; i++) {
-//                    if (grid[i][i] == first) {
-//                        count++;
-//                        if (count == x) {
-//                            return true;
-//                        }
-//                    } else {
-//                        break;
-//                    }
-//                }
-//            }
-//
-//            first = grid[0][m - 1];
-//            if (first != ' ') {
-//                int count = 1;
-//                for (int i = 1; i < n; i++) {
-//                    if (grid[i][m - 1 - i] == first) {
-//                        count++;
-//                        if (count == x) {
-//                            return true;
-//                        }
-//                    } else {
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//        return false;
-//    }
+    public boolean isGameOver(int height, int width) {
+        int index = 0;
+        int ReferenceFieldCondition = gameMap.get(new Pos(height, width));
+        for (int i = -inRowToWin + 1; i < inRowToWin; i++) {
+            if (gameMap.containsKey(new Pos(height + i, width))) {
+                verticalRow[index] = gameMap.get(new Pos(height + i, width)) == ReferenceFieldCondition;
+            }
+            if (gameMap.containsKey(new Pos(height, width + i))) {
+                horizontalRow[index] = gameMap.get(new Pos(height, width + i)) == ReferenceFieldCondition;
+            }
+            if (gameMap.containsKey(new Pos(height - i, width + i))) {
+                firstDiagonalRow[index] = gameMap.get(new Pos(height - i, width + i)) == ReferenceFieldCondition;
+            }
+            if (gameMap.containsKey(new Pos(height + i, width + i))) {
+                secondDiagonalRow[index] = gameMap.get(new Pos(height + i, width + i)) == ReferenceFieldCondition;
+            }
+            index++;
+        }
+        return isEnoughInRow(horizontalRow) || isEnoughInRow(verticalRow) || isEnoughInRow(firstDiagonalRow) || isEnoughInRow(secondDiagonalRow);
+    }
 
     private boolean isEnoughInRow(boolean[] row) {
         int numberInRow = 0;
         for (boolean condition : row) {
-            numberInRow += condition ? 1 : 0;
-            numberInRow *= condition ? 1 : 0;
+            numberInRow += (condition ? 1 : 0);
+            numberInRow *= (condition ? 1 : 0);
+            if (numberInRow >= inRowToWin) {
+                return true;
+            }
         }
-        return numberInRow == inRowToWin;
-    }
-
-    public boolean isGameOver(int xCoord, int yCoord) {
-        boolean[] horizontalRow = new boolean[inRowToWin * 2];
-        boolean[] verticalRow = new boolean[inRowToWin * 2];
-        boolean[] firstDiagonalRow = new boolean[inRowToWin * 2];
-        boolean[] secondDiagonalRow = new boolean[inRowToWin * 2];
-        int value = gameMap. get(new Pair(xCoord, yCoord));
-        for (int i = -inRowToWin; i <= inRowToWin; i++) {
-            verticalRow[i + inRowToWin] = gameMap.containsKey(new Pair(xCoord + i, yCoord)) && gameMap.get(new Pair(xCoord + i, yCoord)) == value;
-            horizontalRow[i + inRowToWin] = gameMap.containsKey(new Pair(xCoord, yCoord + i)) && gameMap.get(new Pair(xCoord, yCoord + i)) == value;
-            firstDiagonalRow[i + inRowToWin] = gameMap.containsKey(new Pair(xCoord - i, yCoord + i )) && gameMap.get(new Pair(xCoord - i, yCoord + i)) == value;
-            secondDiagonalRow[i + inRowToWin] = gameMap.containsKey(new Pair(xCoord + i, yCoord + i )) && gameMap.get(new Pair(xCoord + i, yCoord + i)) == value;
-        }
-        return isEnoughInRow(horizontalRow) || isEnoughInRow(verticalRow) || isEnoughInRow(firstDiagonalRow) || isEnoughInRow(secondDiagonalRow);
+        return false;
     }
 }
--- Main.java
+++ Main.java
@@ -3,34 +3,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter height of game board\n>> ");
-        int height = scanner.nextInt();
-        System.out.print("Enter width of game board\n>> ");
-        int width = scanner.nextInt();
-        System.out.print("Enter a number in a row to win\n>> ");
-        int inRowToWin = scanner.nextInt();
+        int height;
+        int width;
+        int inRowToWin;
+        do {
+            System.out.print("Enter height of game board\n>> ");
+            height = scanner.nextInt();
+        } while (height < 3 || height > 99);
+        do {
+            System.out.print("Enter width of game board\n>> ");
+            width = scanner.nextInt();
+        } while (width < 3 || width > 26);
+        do {
+            System.out.print("Enter a number in a row to win\n>> ");
+            inRowToWin = scanner.nextInt();
+        } while (inRowToWin  < 3 || inRowToWin  >  Math.min(height, width));
         GameGrid gameGrid = new GameGrid(height, width, inRowToWin);
-
         gameGrid.print();
         int stepCount = 0;
         boolean isGameOver = false;
         int maxStepCount = height * width;
-        while (!isGameOver && stepCount != maxStepCount) {
-            //System.out.print("Step " + stepCount + " / " + maxStepCount + '\n');
-            if (stepCount % 2 == 0) {
-                System.out.print("First player step\n>> ");
-            } else {
-                System.out.print("Second player step\n>> ");
-            }
-            height = scanner.nextInt() - 1;
-            width = scanner.nextInt() - 1;
-            gameGrid.tryPut(height, width, stepCount % 2);
-            isGameOver = gameGrid.isGameOver(height, width);
+        do {
+            do {
+                if (stepCount % 2 == 0) {
+                    System.out.print("First player step\n>> ");
+                } else {
+                    System.out.print("Second player step\n>> ");
+                }
+
+                height = scanner.nextInt() - 1;
+                width = scanner.nextInt() - 1;
+            } while (!gameGrid.tryPut(height, width, (stepCount % 2) == 0));
+
             gameGrid.print();
             stepCount++;
-        }
+        } while (!gameGrid.isGameOver(height, width) && stepCount != maxStepCount);
         System.out.print("Game over\n");
-        if (isGameOver) {
+        if (gameGrid.isGameOver(height, width)) {
             if (stepCount % 2 != 0) {
                 System.out.print("First player won");
             } else {
@@ -41,10 +50,3 @@ public class Main {
         }
     }
 }
-
-
-//1 A
-//menu
-//09
-//width < 27, > 0;
-//height < 100, > 0
